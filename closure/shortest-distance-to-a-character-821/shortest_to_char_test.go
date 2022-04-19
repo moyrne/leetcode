752c5f32c1ec31f7e2bf7a735f19b97a944ab44c
moyrn <moyrn@qq.com>
2022-04-19 21:15:31 +0800

    20220419

---
+++ closure/shortest-distance-to-a-character-821/shortest_to_char_test.go
@@ -0,0 +1,42 @@
+package shortest_distance_to_a_character_821
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_shortestToChar(t *testing.T) {
+	type args struct {
+		s string
+		c byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "示例 1",
+			args: args{
+				s: "loveleetcode",
+				c: 'e',
+			},
+			want: []int{3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0},
+		},
+		{
+			name: "示例 2",
+			args: args{
+				s: "aaab",
+				c: 'b',
+			},
+			want: []int{3, 2, 1, 0},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := shortestToChar(tt.args.s, tt.args.c); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("shortestToChar() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
